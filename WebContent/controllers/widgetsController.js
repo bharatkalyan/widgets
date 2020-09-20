5ff7336d31f6ab3b11e57736e1489debf47e5856
bharatkalyan <s.bharatkalyan@gmail.com>
2020-09-20 22:19:24 +0530

    incorporated review comments

--- WebContent/controllers/widgetsController.js
+++ WebContent/controllers/widgetsController.js
@@ -1,106 +1,103 @@
-routerApp.controller('widgetController', function ($scope, $http, $state, $location, $window, $rootScope) {
+angular.module('routerApp').controller('widgetController', function ($scope, $state, $location, WidgetsService, $rootScope) {
 
-    // get method
-    var getallwidgets = function () {
-        $http.get('http://localhost:3000/widgets').success(function (data) {
-            console.log(data, "data testing")
+    // Get widgets
+    var getWidgetsData = function () {
+        WidgetsService.getwidgetDetails().then(function (resp) {
             $scope.widgets = []
-            $scope.widgets = data;
-        }).error(function () {
-            $scope.error = "An Error has occured while loading posts!";
-        });
+            $scope.widgets = resp.data;
+        },
+            function () {
+                alert('Failed');
+            });
+
     }
 
-    // delete method
-    $scope.deleteWidgets = function (inf) {
-        var Id = inf.id;
-        $scope.loading = true;
-        $http.delete('http://localhost:3000/widgets/' + Id).success(function (data) {
-            $location.path('/')
-        }).error(function (data) {
-            $scope.error = "An Error has occured while Delete employee! " + data;
-        });
-        $state.reload()
-    }
-
-
-    // add widgets
-    $scope.addWidgets = function () {
-        $http.post('http://localhost:3000/widgets/', this.newwidgets).success(function (data) {
-            $scope.widgets = data;
-            $scope.newwidgets = '';
-            $location.path('/')
-        }).error(function (data) {
-            $scope.error = "An Error has occured while adding widgets! " + data;
-        });
-        $state.reload()
-    }
-// reset form
-$scope.resetForm = function(){
-	$scope.newwidgets =""
-}
-
-    // show details
+    // get widgets by id
     $scope.showDetails = function (inf) {
-        var id = inf.id
-        //alert("function working")
-        $http.get('http://localhost:3000/widgets/' + id).success(function (data) {
-            console.log(data, "data testing")
-            $scope.widgetsdetails = {}
-            $scope.widgetsdetails = data;
-			$scope.widgetPairs = []
-			$scope.widgetPairs = data.pairs
-		
-        }).error(function () {
-            $scope.error = "An Error has occured while get details!";
+        $scope.widgetsdetails = {}
+        WidgetsService.GetWidgetsByID(inf.id).success(function (data) {
+            $scope.widgetsdetails = data[0];
+            $rootScope.detailsName = data[0].name
+            $rootScope.detailsNumber = data[0].number
+            $rootScope.detailsId = data[0].id
+            $rootScope.detailsPlace = data[0].dob
         });
-        $rootScope.detailsName = inf.name
-        $rootScope.detailsNumber = inf.number
-        $rootScope.detailsId = inf.id
-        $rootScope.detailsPlace = inf.dob
     }
 
-    // edit widget
-    $scope.editWidgets = function () {
-        console.log($rootScope.detailsId)
-        var detName = document.getElementById('name').value
-        var detNumber = document.getElementById('number').value
-        var data = {
-            name: detName,
-            number: detNumber
-        }
-        $http.put('http://localhost:3000/widgets/' + $rootScope.detailsId, data).success(function (data) {
-            console.log(data, "testing edit data")
-            $scope.widgets = data;
-            $location.path('/')
-        }).error(function () {
-            $scope.error = "An Error has occured while loading edit!";
+    // Add widgets
+    $scope.addWidgets = function () {
+        $scope.widgets = this.newwidgets;
+        WidgetsService.AddWidgets($scope.widgets).then(function (d) {
+            $scope.widgets = d;
+            $scope.newwidgets = '';
+            alert("widgets added successfully");
+            $location.path('/');
         });
+        getWidgetsData();
+        $state.reload()
+
+    };
+
+    // Delete Widgets
+    $scope.deleteWidgets = function (inf) {
+        if (inf.id != null) {
+            if (window.confirm('Are you sure you want to delete this Id = ' + inf.id + '?'))//Popup window will open to confirm
+            {
+                WidgetsService.deleteWidgetsData(inf.id).then(function (inf) {
+                    $location.path('/')
+                }, function () {
+                    alert("Error in deleting record");
+                });
+            }
+        }
+        else {
+            alert("this id is null");
+        }
+    };
+
+    // Edit save widget details
+    $scope.saveEditWDetails = function () {
+        var widgetsContent = {}
+        widgetsContent.name = $scope.widgetsdetails.name,
+            widgetsContent.number = $scope.widgetsdetails.number,
+            widgetsContent.id = $rootScope.detailsId
+        widgetsContent = JSON.stringify(widgetsContent)
+        $scope.widId = $rootScope.detailsId;
+        if ($scope.widId != null) {
+            WidgetsService.editWidgetsData(widgetsContent).then(function (widgetsContent) {
+                $scope.widgets = widgetsContent
+                alert("updated successfully")
+                $location.path('/')
+            }, function () {
+                alert("Error in editing record");
+            });
+        }
+        else {
+            alert("this id is null");
+        }
         $state.reload()
     }
 
-// create new input
-var counter = 0;
-$scope.addElement=function() {
-	var form = document.getElementById('editWidget');
-   counter++;
-    var input = document.createElement("input");
-	input.class="col-sm-2"
-    input.id = 'input-' + counter;
-    input.type = 'text';
-    input.name = 'name';
-    form.appendChild(input);
-}
+    // create new input
+    var counter = 0;
+    $scope.addElement = function () {
+        var form = document.getElementById('editWidget');
+        counter++;
+        var input = document.createElement("input");
+        input.class = "col-sm-2"
+        input.id = 'input-' + counter;
+        input.type = 'text';
+        input.name = 'name';
+        form.appendChild(input);
+    }
 
-
-// Remove element
-
-$scope.removeElement = function(inf){
-	var form = document.getElementById('editWidget');
-	var element = document.getElementById(inf.id);
-    element.parentNode.removeChild(form);
-}
-
-
-    getallwidgets();
+    // Remove element
+    $scope.removeElement = function (inf) {
+        var form = document.getElementById('editWidget');
+        var element = document.getElementById(inf.id);
+        element.parentNode.removeChild(form);
+    }
+    getWidgetsData();
 });
+
+
--- WebContent/factory/widgetsFactory.js
+++ WebContent/factory/widgetsFactory.js
@@ -1,27 +1,76 @@
-var baseAddress = 'http://localhost:3000/widgets/';
- var url = "";
+var baseURl = "http://localhost:3000/widgets/";
+angular.module('routerApp').factory('WidgetsService', function ($http, $q, $window) {
+    return {
+        getwidgetDetails: function () {
+            return $http.get('baseURl');
+        },
 
-routerApp.factory("widgetsFactory", function($http,$q){
-return{
-	getWidgetsList: function () {
- url = baseAddress;
- return $http.get(url);
- },
+        // Get Widgets By Id
+        GetWidgetsByID: function (widgetsId) {
+            if (widgetsId != null) {
+                console.log(widgetsId, 'widgetsId')
+                return $http.get('baseURl', { params: { id: widgetsId } });
+            }
+        },
+        //delete widgets
 
-deleteWidgetUser : function (employeeid) {
-        var defer = $q.defer(); // I have disscussed littel bit about $q and defer in 3rd tutorial of angualrjs. 
-        $http({
-            url: baseAddress +employeeid, 
-            method: 'DELETE'
-        }).success(function (d) {
-            alert("the widgets deleted successfully");
-            defer.resolve(d);
-        }).error(function (e) {
-            alert("Error");
-            defer.reject(e);
-        });
-        return defer.promise;
+        deleteWidgetsData: function (widgetsId) {
+            var defer = $q.defer();
+            $http({
+                url: 'baseURl' + widgetsId,
+                method: 'DELETE'
+            }).success(function (d) {
+                alert("widgets deleted successfully");
+                defer.resolve(d);
+            }).error(function (e) {
+                alert("Error");
+                defer.reject(e);
+            });
+            return defer.promise;
 
-    },
-}
-});
+        },
+
+        // Edit Widgets
+
+        editWidgetsData: function (widgetsContent) {
+            var defer = $q.defer();
+            var testCont = JSON.parse(widgetsContent);
+            console.log(testCont)
+            var Id = testCont.id
+            $http({
+                url: 'baseURl' + Id,
+                method: "PUT",
+                data: widgetsContent,
+                headers: { 'Content-Type': 'application/json' },
+            }).success(function (d) {
+                defer.resolve(d);
+            }).error(function (e) {
+                console.log(e, 'error')
+                alert("Error");
+                defer.reject(e);
+            });
+            return defer.promise;
+        },
+
+        // add widgets
+
+        AddWidgets: function (data) {
+            var defer = $q.defer();
+            $http({
+                url: 'baseURl',
+                method: "POST",
+                data: data
+            }).success(function (d) {
+                //Callback after success
+                defer.resolve(d);
+            }).error(function (e) {
+                //callback after failed
+                alert("Error");
+
+                defer.reject(e);
+            });
+            return defer.promise;
+        }, //End of Add widgets Data
+    }
+})
+
